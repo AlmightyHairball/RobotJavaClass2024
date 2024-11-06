a4cd46a6e59ff23363498bdaa0bf7caffcaa7133
AbstractestThought <quinn.kvochick@gmail.com>
2024-11-06 18:00:57 -0500

    added API link for CANSparkMax

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -18,7 +18,12 @@ public final class Main {
   public static void main(String... args) {
     //RobotBase.startRobot(Robot::new);
     //Workspace.main();
+    //-------------------------------------------
     Spaceship.main();
+
+
+    
+    //-------------------------------------------
   }
 
 }
